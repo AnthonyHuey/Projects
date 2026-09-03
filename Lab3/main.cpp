331d3cd6a5c3ce67447d7361e0cd443b332c79f1
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-03 13:41:20 -0700

    Setting up the uInput function/remeberd to save this time

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -7,16 +7,31 @@
 using namespace std;
 
 // Struct Template
-
+struct Restaurant
+{
+    string name;
+    int chairs;
+    int tables;
+    bool open;
+};
 // Function for filling struct with user data
-
+Restaurant uInput();
 // Function for outputting struct to console
+void output(const Restaurant&);
 
 int main()
 {
 
    // Make the struct,
-
+   Restaurant test = uInput();
    // Output the struct.
-   
+
+}
+Restaurant uInput()
+{
+
+}
+void output(const Restaurant&)
+{
+
 }
