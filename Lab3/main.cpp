b3093d80fc53ee55e6a6c280491ff0a8c67b6099
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-03 13:29:07 -0700

    Initial template

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,10 +1,22 @@
+// Anthony Huey - 9/3/2026 - Lab3 Resaurant Structs
+//create a struct, fill it with user data, output to console
+
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+// Struct Template
+
+// Function for filling struct with user data
+
+// Function for outputting struct to console
 
 int main()
 {
 
-   
+   // Make the struct,
 
+   // Output the struct.
+   
 }
