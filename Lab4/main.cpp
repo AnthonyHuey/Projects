7fda6f535b5c0f4359814705d7b2794589c574df
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-05 12:38:23 -0700

    reviewing vectors

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -21,7 +21,8 @@ int main()
     cout << test.r << " " << test.g << " " << test.b << "\n\n";
 
     // create a struct vector.
-
+    vector<Color> list {(75,50,25)};
+    cout << list.r <<
     // fill it with random data
 
     // output
