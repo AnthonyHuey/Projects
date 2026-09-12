a4cc50e2324ca6244492e513636300d8e3790ee9
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-12 14:28:30 -0700

    Final commit, seems good.

--- Lab6/Main.cpp
+++ Lab6/Main.cpp
@@ -10,60 +10,26 @@ using namespace std;
 double* enterArrayData();
 
 // Output the arrays data on a single line
-//double outputArrayData(double[]);
+void outputArrayData(double[]);
 
 // Add all the values of the array up and output the sum
 void sumArray(double[]);
 // Max size of the array
+
 const int MAX = 5;
 
 int main()
 {
-    // Create the array/Fill it with data
-
-    // Output
-
-    // Sum
-    
-    // quick tests
-    /*
-    double array[MAX] = {1,2,3,4,5};
-
-    double *ptr = nullptr; 
-
-    ptr = array;
-
-    for (int i = 0; i < MAX; i++)
-        cout << "List: " << ptr[i] << " ";
-   
-    for (int i = 0; i < MAX; i++)
-        cout <<"\n Another list: " << *(ptr + i) << " ";
-    
-    cout << "\n\nDYNAMIC MEMORY\n\n";
-
-    double *newptr = nullptr;
-    newptr = new double[MAX];
-
-    for (int i = 0; i < MAX; i++)
-        newptr[i] = i + 1;
-
-    cout << "List: "; 
-    for (int i = 0; i < MAX; i++)
-        cout << newptr[i] << " ";
-
-    //passing the tests to a function now
-    cout << "\nTESTING SUM FUNCTION\n";
-    sumArray(newptr);
-
-    delete newptr;
-    */
-
     double *testptr = nullptr;
-    testptr = enterArrayData();
-    sumArray(testptr);
+    testptr = enterArrayData();         // Create/fill array
+    cout << "Data entry complete. ";
+    outputArrayData(testptr);           // Output 
+    cout << "\n";
+    sumArray(testptr);                  // Sum
 
-    delete [] testptr;
+    delete [] testptr;                  // never forget to delete.
 }
+
 double* enterArrayData()
 {
     double *array = nullptr;
@@ -75,10 +41,18 @@ double* enterArrayData()
         cout << "Element#" << i << ": ";
         cin >> entry;
         cin.ignore();
-        array[i] = entry; 
+        *(array + i) = entry; 
     }
     return array;
 }
+
+void outputArrayData(double array[])
+{
+    cout << "Outputting array elements: ";
+    for (int i = 0; i < MAX; i++)
+        cout << *(array + i) << " ";
+}
+
 void sumArray(double array[])
 {
     double sum = 0;
