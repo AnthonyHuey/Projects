d9e36fc6c478b4d5bdc9b6f383b4e115d333debc
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-12 14:46:28 -0700

    initial template

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -1,3 +1,35 @@
 // Anthony Huey - 9/12/2026 - Lab 7: Dynamic Arrays & Functions 2
 // create a dynamic string array, call functions to reverse and output.
-// POINTER NOTATION
+// POINTER NOTATION, surely it's not much harder than doubles right.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Reverses the elements of the array
+string* reverseArray(string[]); 
+
+// Output array.
+void displayArray(string[]);
+
+const int MAX = 5;
+
+int main()
+{
+   // Create/populate the array
+
+   // Reverse it
+
+   // Output it
+}
+
+string* reverseArray(string array[])
+{
+
+}
+
+void displayArray(string array[])
+{
+
+}
