71f293f442115502beaf7dabd2b4026d46eef54a
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-18 13:43:08 -0700

    why did my array break?

--- Lab9/Main.cpp
+++ Lab9/Main.cpp
@@ -13,10 +13,10 @@ const int MAX = 30;
 
 int main()
 {
-    // Create the STD::array
-    array<int, MAX> list;
+// Create the STD::array
+    vector<int, MAX> list;
         
-    // Fill it with fin
+// Fill it with fin
     ifstream fin;
     fin.open("list.txt");
     if (fin.good())
@@ -30,13 +30,13 @@ int main()
 
     for (int val : list) cout << val << " ";
 
-    // have some fun with <array> functions
+// have some fun with <array> functions
     cout << "\nEmpty? " << (list.empty() == 0? "False" : "True"); // neat
     cout << "\nFirst: " << list.front();
     cout << "\nLast: " << list.back();
     cout << "\nAddress? " << list.data();
 
-    // sets r to a random #, get the # from array at r
+// sets r to a random #, get the # from array at r
     srand(time(0));
     int r = 0;
     for (int i = 0; i < 5; i++) 
@@ -60,11 +60,65 @@ int main()
     else
         cout << " was not found.";
 
-    int total = 0;
+//not using #include <numeric> for accumulate.
+    int total = 0; 
     for (int i = 0; i < MAX; i++)
         total = list.at(i) + total;
     cout <<  "\nTotal of all #'s is: " << total;
 
-    // do it all again for <vector>
+// do it all again for <vector>
+
+// Create the STD::array
+    vector<int> table;
+        
+// Fill it with fin
+    ifstream fin;
+    fin.open("list.txt");
+    if (fin.good())
+    {
+       for (int i = 0; i < table.size(); i++)
+            fin >> table.at(i);
+        fin.close();  
+    }    
+    else
+        cout << "File not found.\n";
+
+    for (int val : table) cout << val << " ";
+
+// have some fun with <array> functions
+    cout << "\nEmpty? " << (table.empty() == 0? "False" : "True"); // neat
+    cout << "\nFirst: " << table.front();
+    cout << "\nLast: " << table.back();
+    cout << "\nAddress? " << table.data();
+
+// sets r to a random #, get the # from array at r
+    srand(time(0));
+    int r = 0;
+    for (int i = 0; i < 5; i++) 
+        cout << "\nElement " << ((r = rand() % MAX) + 1) << ": " << table.at(r);
+
+    cout << "\n\t-----SORTING-----\t\n";
+    sort(table.begin(), table.end());
+    cout << "sorted.";
+    for (int val : table) cout << val << " ";
+
+    int search = 0;
+    cout << "\nWhat # would you like to search for: ";
+    cin >> search;
+    cin.ignore();
+
+    vector<int>::iterator iter;
+    iter = find(table.begin(), table.end(), search);
+    cout << "Value " << search;
+    if (iter != table.end())
+        cout << " found in position " << iter - table.begin();
+    else
+        cout << " was not found.";
+
+//not using #include <numeric> for accumulate.
+    int total = 0; 
+    for (int i = 0; i < table.size(); i++)
+        total = table.at(i) + total;
+    cout <<  "\nTotal of all #'s is: " << total;
 
 }
