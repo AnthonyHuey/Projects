3dc93f8247a530cde7d1f7aac1e4f0d2a29e9e3d
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-20 19:03:45 -0700

    more setup

---
+++ Lab11/main.cpp
@@ -0,0 +1,27 @@
+// Anthony Huey - 9/20/2026 - COMSC 210 - Pointers 2, Dynamic array of structs.
+// Initial idea is a car rental service for the struct 
+//      First/Last name, 
+//      VIN of the car rented?
+//      How many days to rent?
+// need to think about what the dynamic array in the struct will be
+
+#include <array>
+#include <iostream>
+
+using namespace std;
+
+
+struct Customer 
+{
+   string fName;
+   string lName;
+   int days;
+   //some dynamic array idk
+};
+
+int main()
+{
+
+
+
+}
