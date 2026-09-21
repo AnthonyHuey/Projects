b6d13f80baee33f60a9696543c4c8fd92c4a433e
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-20 20:01:53 -0700

    Seems good, fixed the silly for loop bug in that last loop

--- Lab11/main.cpp
+++ Lab11/main.cpp
@@ -1,9 +1,9 @@
 // Anthony Huey - 9/20/2026 - COMSC 210 - Pointers 2, Dynamic array of structs.
 // Initial idea is a car rental service for the struct 
-//      First/Last name, 
+//      Name,
+//      # of cars rented 
 //      VIN of the car rented?
-//      How many days to rent?
-// need to think about what the dynamic array in the struct will be
+//  I know VIN aren't just numbers but were keeping it simple.
 
 #include <iostream>
 #include <string>
@@ -29,33 +29,32 @@ int main()
     // build the array
     Customer *customers = new Customer[c]; 
 
-    // Get the customer name, how many cars, then create the array for the VINs.
+    // Get the customer name, how many cars.
     for (int i = 0; i < c; i++)
     {
-        cout << "\nCustomer #" << (i+1) << " name: ";
+        cout << "\nCustomer #" << (i+1) << "'s name: ";
         getline(cin, customers[i].name);
 
-        cout << "How many cars did they rent?";
+        cout << "How many cars did they rent: ";
         cin >> customers[i].count;
         cin.ignore();
 
-        // create array based on how many cars rented
+        // create the struct array based on how many cars rented, then fill it.
+        customers[i].vin = new int[customers[i].count];
+
         for (int n = 0; n < customers[i].count; n++)
         {
             cout << " VIN of car #" << (n + 1) << ": ";
             cin >> customers[i].vin[n];
+            cin.ignore();
         }
     }
-    
-    cout << "\n\nDid it work?\n\n";
+    // Output to check work.
     for (int i = 0; i < c; i++)
     {
-        cout << customers[i].name << " rented " << customers[i].count
-             << " car(s)." << "\nCar(s) VIN is:";
-        for (int n = 0; n < customers[i].count; i++)
+        cout << "\n" << customers[i].name << " rented " << customers[i].count
+             << " car(s)." << "\nCar(s) VIN is: ";
+        for (int n = 0; n < customers[i].count; n++)
             cout << "\n#" << customers[i].vin[n];
     }
-
-
-
 }
