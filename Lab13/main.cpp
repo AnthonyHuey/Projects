bff113c5f5e5425f55eb00ec026d2b451b820d77
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-25 16:06:16 -0700

    working through the file input

--- Lab13/main.cpp
+++ Lab13/main.cpp
@@ -3,20 +3,41 @@
 // Write the results to a file, along with a summary.
 
 #include <iostream>
+#include <fstream>
 #include <array>
 
 using namespace std;
 
+// Hold the Input file data
 struct Student
 {
     int id;
     double grade;
 };
 
+const int MAX = 255; // just for testing now, probably
+
+
 int main()
 {
+    // create array
+    array<Student, MAX> list;
+    // fill array with data
+    ifstream fin;
+    fin.open ("210-lab-13-grades.txt");
+    if (fin.good())
+    {
+        for (int i = 0; i < MAX; i ++)
+        {   
+            
+            
+        }
+    }
+    else 
+        cout << "File not found.\n";
+    // sort array
 
-
+    // output array
 
     
 }
