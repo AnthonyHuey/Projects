35d7132d49eb13410ac7c841fde070a1918571d3
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-25 15:54:45 -0700

    more setup

--- Lab13/main.cpp
+++ Lab13/main.cpp
@@ -1,2 +1,22 @@
 // Anthony Huey - 9/25/2026 - COMSC 210 - Lab13: Student Grade Sorter
-//
+// Sort a list of student grades into an array of structs, ID/Score
+// Write the results to a file, along with a summary.
+
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+struct Student
+{
+    int id;
+    double grade;
+};
+
+int main()
+{
+
+
+
+    
+}
