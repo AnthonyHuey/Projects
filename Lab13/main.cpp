fa5de42d53a43c9de78e7e9feac3c966693b21a3
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-25 16:55:21 -0700

    nevermind, my cout was wack, why did i do that

--- Lab13/main.cpp
+++ Lab13/main.cpp
@@ -61,7 +61,7 @@ int main()
         }
     // did it work?
     cout << "\n\n ID \t GRADE ";
-    for (int i = 0; i < (list.size() - 1); i++)
+    for (int i = 0; i < (list.size()); i++)
         cout << "\n" << list[i].id << "\t" << list[i].grade;
 
     // output array
