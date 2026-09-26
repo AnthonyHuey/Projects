38e3a2908944ca6043d6cb3ddc11186eee3eeea0
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-26 08:29:24 -0700

    initial commit

---
+++ Lab14/main.cpp
@@ -0,0 +1,5 @@
+// Anthony Huey - 9/26/2026 - COMSC 210 - Lab14: Color Class
+// Create a Color class that has 3 private RGB values
+// Setter/Getter and print() member functions
+// Make a few in main, test them out.
+
