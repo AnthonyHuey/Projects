26bb80ac2e88298401c69add97d79a0397dc0aa7
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-26 09:10:44 -0700

    Setting things up

--- Lab15/main.cpp
+++ Lab15/main.cpp
@@ -3,3 +3,44 @@
 // same set/get adn print member funcs
 // gets data from input file, Title - Year - Name
 // put that data into an object then into the class
+
+#include <iostream>
+#include <array>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// class for movie data
+class Movie
+{
+private:
+    string title;
+    int year;
+    string name;
+public:
+    void setTitle(string t) {};
+    void setYear(int y) {};
+    void setName(string n) {};
+
+    string getTitle() const {};
+    int getYear() const {};
+    string getName() const {};
+
+    void print() const {};
+
+};
+struct Input
+{
+    string title;
+    int year;
+    string name;
+};
+
+int main()
+{
+
+
+
+
+}
