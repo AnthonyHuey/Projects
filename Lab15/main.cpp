6586e19d92ad665405257b3c6c061b8f494d2609
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-26 08:59:39 -0700

    Initial commit

---
+++ Lab15/main.cpp
@@ -0,0 +1,5 @@
+// Anthony Huey - 9/26/2026 - COMSC 210 - Lab15: Movie Class
+// Movie class, screen writer / yaer released / title
+// same set/get adn print member funcs
+// gets data from input file, Title - Year - Name
+// put that data into an object then into the class
