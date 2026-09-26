dabbe4e7bb1a01981dd9c988deb3eab747ac52ac
Anthony Huey <anthonyjhuey@gmail.com>
2026-09-26 08:39:04 -0700

    Took COMSC 200, this is easy peasy

--- Lab14/main.cpp
+++ Lab14/main.cpp
@@ -3,3 +3,34 @@
 // Setter/Getter and print() member functions
 // Make a few in main, test them out.
 
+#include <iostream>
+
+using namespace std;
+
+class Color
+{
+private:
+    int red;
+    int green;
+    int blue;
+public:
+   void setRed(int r) {red = r;}
+   void setGreen(int g) {green = g;}
+   void setBlue(int b) {blue = b;}
+
+   int getRed() const {return red;}
+   int getGreen() const {return green;}
+   int getBlue() const {return blue;}
+
+   void print() const {cout << "Red: " << red << "\nGreen: " << green 
+                            << "\nBlue: " << blue;} 
+
+};
+
+int main()
+{
+
+
+
+
+}
